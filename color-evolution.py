974c7ac35f40f29c780f79b28448fdfdf9deb786
fcarlosmonteiro <fcarlosmonteiro@gmail.com>
2021-08-20 14:12:16 -0300

    algorithm architecture

---
+++ color-evolution.py
@@ -0,0 +1,18 @@
+#get an image and ex    1tract a set of colors
+def get_colors(path):
+    return colors
+
+def compute_contrast_ratio(colorA, colorB):
+    return True or False
+
+def get_initial_solution():
+    return solution
+
+def generate_neighborhood():
+    return neighborhood
+
+def main():
+    print()
+
+if __name__ == "__main__":
+    main()
