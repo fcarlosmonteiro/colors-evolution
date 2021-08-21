887841170089972a15260f853fae52028e09e2f7
fcarlosmonteiro <fcarlosmonteiro@gmail.com>
2021-08-21 15:33:46 -0300

    algorithm update

--- color-evolution.py
+++ color-evolution.py
@@ -8,10 +8,21 @@ def compute_contrast_ratio(colorA, colorB):
 def get_initial_solution():
     return solution
 
-def generate_neighborhood():
+def generate_neighborhood(current):
     return neighborhood
 
+def objective_function(solution):
+    return fit_value
+
 def main():
+    #get first solution
+    
+    #solution becomes current
+    #extends neighborhood
+    #evaluate solutions
+    #select best solution
+    #best solution becomes current
+    #back to line 20
     print()
 
 if __name__ == "__main__":
