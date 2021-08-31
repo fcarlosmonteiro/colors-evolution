4f66e0a7537e3c4cdffc7ff45b600d693b189260
fcarlosmonteiro <fcarlosmonteiro@gmail.com>
2021-08-31 01:05:36 -0300

    hill climbing

--- color-evolution.py
+++ color-evolution.py
@@ -8,6 +8,7 @@ from colormath.color_diff import delta_e_cie2000
 from networkx.classes.function import neighbors
 import wcag_contrast_ratio as contrast
 
+
 #get an image and extract a set of colors
 def get_colors(path):
     colors, pixel_count = extcolors.extract_from_path(path)
@@ -26,7 +27,6 @@ def rgb2hsv(colors):
     
         #get hsv percentage: range (0-1, 0-1, 0-1)
         color_hsv_percentage=colorsys.rgb_to_hsv(red_percentage, green_percentage, blue_percentage) 
-        #print('color_hsv_percentage: ', color_hsv_percentage)
     
         #get normal hsv: range (0-360, 0-255, 0-255)
         color_h=round(360*color_hsv_percentage[0])
@@ -35,7 +35,6 @@ def rgb2hsv(colors):
         color_hsv=(color_h, color_s, color_h)
         colors_hsv.append(color_hsv_percentage)
     return colors_hsv
-    #print('color_hsv: ', color_hsv)
 
 def find_base(colors):
     for color in colors:
@@ -53,9 +52,14 @@ def find_base(colors):
 
 def convert_scale255(color):
     result=[]
-    for x in color:
-        r=x*255.0
-        result.append(round(r,2))
+    try:
+        for x in color:
+            r=x*255.0
+            result.append(round(r,2))
+    except:
+        for x in color[0]:
+            r=x*255.0
+            result.append(round(r,2))
     return result
 
 def convert_scale(color):
@@ -67,17 +71,33 @@ def convert_scale(color):
     
 #recebe a cor base e o vetor de cores
 def compute_contrast_ratio(corBase, colors):
-    for c in colors:
+    global result
+    findcolor=False
+    result=[]
+    for idx,c in enumerate(colors):
         initial_color=convert_scale(c[0])
         ratio=contrast.rgb(corBase, initial_color)
-        #print("Cor 1:"+str(corBase)+" e Cor 2:"+str(initial_color)+" - contrast ratio = "+str(round(ratio,2)))
+        
         valueWCAG=contrast.passes_AA(ratio)
+        #print("Cor 1:"+str(corBase)+" e Cor 2:"+str(initial_color)+" - contrast ratio = "+str(round(ratio,2))+ " contrast test: ",valueWCAG)
         if valueWCAG==False:
-            #print("Cor 1:"+str(corBase)+" e Cor 2:"+str(c)+" - contrast ratio = "+str(round(ratio,2))+ " contrast test: ",valueWCAG)
+            #print("Cor 1:"+str(corBase)+" e Cor 2:"+str(initial_color)+" - contrast ratio = "+str(round(ratio,2))+ " contrast test: ",valueWCAG)
             print("Hill Climbing algorithm is starting...")
-            return initial_color,ratio
+            hill_climbing(initial_color,ratio,corBase,findcolor)
+            #return initial_color,ratio
         else:
+            print("=============Color "+str(idx)+" passed the AA test=============")
+            result.append(initial_color)
+
+def hill_climbing(initial_color,ratio,corBase,findcolor):
+    for i in range(1,50000):
+        #print("...")
+        neighborhood=generate_neighborhood(initial_color)
+        initial_color, findcolor=objective_function(initial_color,ratio,corBase,neighborhood)
+        if findcolor==False:
             pass
+        else:
+            break
 
 def generate_neighborhood(current):
     neighborhood=[]
@@ -90,37 +110,46 @@ def generate_neighborhood(current):
     #print(neighborhood)
     return neighborhood
 
-def objective_function(current,colorBase,neighborhood):
+def objective_function(initial_color,ratio,corBase,neighborhood):
     for n in neighborhood:
         n=convert_scale(n)
-        ratio=contrast.rgb(colorBase, n)
-        valueWCAG=contrast.passes_AA(ratio)
-        print("Base color:"+str(colorBase)+" and neighbor:"+str(n)+" - contrast ratio = "+str(round(ratio,2))+ " contrast test: ",valueWCAG)
-        obj_value=ratio
-        if current[1]<obj_value:
+        ratioNeighbor=contrast.rgb(corBase, n)
+        valueWCAG=contrast.passes_AA(ratioNeighbor)
+        #print("**Base color:"+str(corBase)+" and neighbor:"+str(n)+" - contrast ratio = "+str(round(ratioNeighbor,2))+ " contrast test: ",valueWCAG)
+        obj_value=ratioNeighbor
+        if valueWCAG==True:
             #n=convert_scale255(n)
-            current=n,obj_value
-    return current
+            initial_color=n,obj_value
+            result.append(n)
+            findcolor=True
+            print("=============Color passed the AA test=============")
+            return initial_color,findcolor
+        else:
+            findcolor=False
+            new=find_nearest_neighbor(ratio,obj_value)
+            if new == ratio:initial_color=initial_color
+            else:initial_color=n
+            return initial_color,findcolor
+
+def find_nearest_neighbor(a,b):
+    reference=4.5
+    a_list=[a,b]
+    absolute_difference_function = lambda list_value : abs(list_value - reference)
+    closest_value = min(a_list, key=absolute_difference_function)
+    return closest_value
+
 
 def main():
-    path="logo2.png"
+    path="logo.png"
     colors=get_colors(path)
     #print("colors",colors)
     colors_hsv=rgb2hsv(colors)
     colorBase=find_base(colors_hsv)
-    #print(colorBase)
-    
-    #initial solution have the color and ratio
-    initial_sol=compute_contrast_ratio(colorBase, colors)
-    current=initial_sol
-
-    for i in range(1,50):
-        #print("------------->",current)
-        neighborhood=generate_neighborhood(current[0])
-        #neighborhood=[[0,0,0]]
-        current=objective_function(current,colorBase,neighborhood)
-       
-    print()
+    compute_contrast_ratio(colorBase, colors)
+    print("Cores que passaram no teste: "+str(result))
 
 if __name__ == "__main__":
-    main()
+    for interation in range(1,2):
+        print("------------------------------Repetition: "+str(interation)+"------------------------------")
+        main()
+        
